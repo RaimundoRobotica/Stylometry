8dd124348c3ed27a93ebc223c9cf993e68507d24
RaimundoRobotica <raimundo.robotica@gmail.com>
2023-11-26 19:44:08 +0100

    New file structure

--- src/read_data.py
+++ src/read_data.py
@@ -9,17 +9,17 @@ def read_text(address, title, author=None):
 
 def create_text_list():
     texts_list = []
-    texts_list.append(read_text('data/Galdos_Fortunata_y_Jacinta.txt', 'Fortunata y Jacinta'))
+    texts_list.append(read_text('data/Galdos_Fortunata_y_Jacinta.txt', 'Fortunata y Jacinta', author='Benito Pérez Galdós'))
     texts_list.append(read_text('data/Galdos_Gerona.txt', 'Gerona', author='Benito Pérez Galdós'))
     texts_list.append(read_text('data/Galdos_La_corte_de_Carlos_IV.txt', 'La corte de Carlos IV', author='Benito Pérez Galdós'))
     texts_list.append(read_text('data/Galdos_La_de_Bringas.txt', 'La de Bringas', author='Benito Pérez Galdós'))
-    texts_list.append(read_text('data/Galdos_Marianela.txt', 'Marianela', author='Benito Pérez Galdós'))
+    texts_list.append(read_text('data/Galdos_Marianela.txt', 'Marianela'))
     texts_list.append(read_text('data/Galdos_Misericordia.txt', 'Misericordia', author='Benito Pérez Galdós'))
     texts_list.append(read_text('data/Galdos_Napoleon_en_Chamartin.txt', 'Napoleón en Chamartín', author='Benito Pérez Galdós'))
     texts_list.append(read_text('data/Galdos_Trafalgar.txt', 'Trafalgar', author='Benito Pérez Galdós'))
     texts_list.append(read_text('data/Pardo_Bazan_La_prueba.txt', 'La prueba', author='Emilia Pardo Bazán'))
     texts_list.append(read_text('data/Pardo_Bazan_La_Tribuna.txt', 'La tribuna', author='Emilia Pardo Bazán'))
-    texts_list.append(read_text('data/Pardo_Bazan_Los_pazos_de_Ulloa.txt', 'Los pazos de Ulloa'))
+    texts_list.append(read_text('data/Pardo_Bazan_Los_pazos_de_Ulloa.txt', 'Los pazos de Ulloa', author='Emilia Pardo Bazán'))
     texts_list.append(read_text('data/Pardo_Bazan_Un_viaje_de_novios.txt', 'Un viaje de novios', author='Emilia Pardo Bazán'))
     texts_list.append(read_text('data/Pardo_Bazan_Una_Cristiana.txt', 'Una cristiana', author='Emilia Pardo Bazán'))
     texts_list.append(read_text('data/Valera_Algo_de_todo.txt', 'Algo de todo', author='Juan Valera'))
@@ -27,8 +27,8 @@ def create_text_list():
     texts_list.append(read_text('data/Valera_Doña_Luz.txt', 'Doña_luz', author='Juan Valera'))
     texts_list.append(read_text('data/Valera_Genio_y_figura.txt', 'Genio y figura', author='Juan Valera'))
     texts_list.append(read_text('data/Valera_Juanita_la_Larga.txt', 'Juanita_la_Larga', author='Juan Valera'))
-    texts_list.append(read_text('data/Valera_Morsamor.txt', 'Morsamor', author='Juan Valera'))
+    texts_list.append(read_text('data/Valera_Morsamor.txt', 'Morsamor'))
     texts_list.append(read_text('data/Valera_Pasarse_de_listo.txt', 'Pasarse de listo', author='Juan Valera'))
-    texts_list.append(read_text('data/Valera_Pepita_Jimenez.txt', 'Pepita Jiménez'))
+    texts_list.append(read_text('data/Valera_Pepita_Jimenez.txt', 'Pepita Jiménez', author='Juan Valera'))
     
     return texts_list
